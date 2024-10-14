7d73e2571e777e7895c1886f323ae24401cbd5bf
madmax <maksimkholin@gmail.com>
2024-10-14 14:03:56 +0300

    add screen1

--- app/src/main/java/com/example/crosswordsolver/MainActivity.kt
+++ app/src/main/java/com/example/crosswordsolver/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.crosswordsolver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.crosswordsolver.ui.theme.CrosswordSolverTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,13 +13,5 @@ class MainActivity : ComponentActivity() {
                 CrosswordSolverApp()
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CrosswordSolverTheme {
-        CrosswordSolverApp()
     }
 }
